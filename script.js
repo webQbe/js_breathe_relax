d2f469d86b99e7e7be936e980ac21e4c19f7e423
webQbe <webcubesolutionslk@gmail.com>
2024-11-14 11:57:43 +0530

    Defined breatheAnimation()

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ const holdTime = totalTime / 5;
 const container = document.querySelector('.container');
 const text = document.querySelector('#text');
 
+function breatheAnimation(){
+
+    // Display Status
+    text.innerHTML = 'Breath In!';
+
+    // Add Class To Container
+    container.className = 'container grow';
+
+    // Update Status After breatheTime
+    setTimeout(() => {
+
+        text.innerHTML = 'Hold';
+
+        // Update Status & 
+        // Add Class to Container After holdTime
+        setTimeout(() => {
+
+            text.innerHTML = 'Breathe Out!';
+            container.className = 'container shrink';
+
+        }, holdTime);
+
+    }, breatheTime);
+
+}
+
